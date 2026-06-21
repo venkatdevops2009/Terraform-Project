195185c1e075ca3daf859867112e9e0fbe1764bb
venkatdevops2009 <venkatdevops2009@gmail.com>
2026-06-21 17:20:30 +0530

    Rename output variables in output.tf

--- Infra/output.tf
+++ Infra/output.tf
@@ -1,7 +1,7 @@
-output "Java Server Public IP" {
+output "java_server_public_ip" {
   value = aws_instance.java_server.public_ip
 }
 
-output "DB Server Private IP" {
+output "db_server_private_ip" {
   value = aws_instance.db_server.private_ip
 }
