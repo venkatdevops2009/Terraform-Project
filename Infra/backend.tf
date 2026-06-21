8ab2f791b8f95d7c9332f541f236ae91b7e7618c
Venkat <venkatdevops2009@gmail.com>
2026-06-21 19:06:25 +0530

    added infra & ansible code

--- Infra/backend.tf
+++ Infra/backend.tf
@@ -2,7 +2,7 @@ terraform {
   backend "s3" {
     bucket       = "petclinic1805"
     key          = "terraform-jenkins-tf-state"
-    region       = "ap-south-1"
+    region       = "us-east-1"
     use_lockfile = true
   }
 }
